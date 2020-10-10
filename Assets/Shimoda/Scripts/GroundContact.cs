d486b9c35addd1a31d7e0072337246d750f8abb2
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2020-10-10 13:04:15 -0400

    GroundTagArray

--- Assets/Shimoda/Scripts/GroundContact.cs
+++ Assets/Shimoda/Scripts/GroundContact.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class GroundContact : MonoBehaviour
 {
+    public string[] groundTags;
     private bool isGrounded = true;
+    private string groundie;
 
+    void Start(){
+        foreach(string gnd in groundTags){
+            groundie += "|" + gnd + "|"; 
+        }
+    }
     public bool Check(){
         return isGrounded;
     }
 
     private void OnTriggerEnter(Collider other){
         Debug.Log("TriggerEnter: " + other.tag);
-        if(other.tag.Equals("Ground")){
+        if(groundie.IndexOf("|" + other.tag + "|") >=0){
             Debug.Log("You're grounded");
             isGrounded = true;
         }
     }
     private void OnTriggerExit(Collider other){
         Debug.Log("TriggerExit: " + other.tag);
-        if(other.tag.Equals("Ground")){
+        if(groundie.IndexOf("|" + other.tag + "|") >=0){
             Debug.Log("Floating in the air");
             isGrounded = false;
         }
