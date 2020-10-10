e5fd093378cb1d65413e8ee998f5c5532969c603
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2020-10-09 22:29:56 -0400

    basic movement back and forth

---
+++ Assets/Shimoda/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    bool movingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        if(direction != 0){
+            if (direction > 0 && ! movingRight){
+                movingRight = true;
+                transform.Rotate(new Vector3(0,180,0));
+            }
+            if (direction < 0 && movingRight){
+                movingRight = false;
+                transform.Rotate(new Vector3(0,180,0));
+            }
+            transform.Translate(new Vector3(0, 0, Mathf.Abs(direction)));
+        }
+
+    }
+}
