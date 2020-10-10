d63fc8b963209ef3a8eaa2d5b240f8b64c929c18
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2020-10-10 11:24:07 -0400

    Small changes

--- Assets/Shimoda/Scripts/PlayerMovement.cs
+++ Assets/Shimoda/Scripts/PlayerMovement.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    bool movingRight = true;
+    public float jumpForce;
+    private bool movingRight = true;
+    private Rigidbody myRigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var character = transform.Find("Character");
+        myRigidbody = character.GetComponent<Rigidbody>();
+        if(speed <= 0) speed = 1f;
+        if(jumpForce <= 0) jumpForce = 0.4f;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void MoveHorizontal(){
         float direction = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         if(direction != 0){
             if (direction > 0 && ! movingRight){
@@ -27,6 +31,15 @@ public class PlayerMovement : MonoBehaviour
             }
             transform.Translate(new Vector3(0, 0, Mathf.Abs(direction)));
         }
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        MoveHorizontal();
+        if(Input.GetButton("Jump")){
+            myRigidbody.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+
+        }
     }
 }
