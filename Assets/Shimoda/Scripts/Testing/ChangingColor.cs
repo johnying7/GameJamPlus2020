875f281fafd402816f0e1636454a6f7f516a8b22
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2020-10-10 22:09:25 -0400

    Requires sync with animation

---
+++ Assets/Shimoda/Scripts/Testing/ChangingColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingColor : MonoBehaviour
+{
+    public Material[] materialList;
+    private bool activeMaterial = false;
+    public void ChangeColor(){
+        Debug.Log("test");
+        if(activeMaterial){
+            activeMaterial = false;
+            this.GetComponent<Renderer>().material = materialList[0];
+        } else {
+            activeMaterial = true;
+            this.GetComponent<Renderer>().material = materialList[1];
+
+        }
+    }
+}
