2866ca3409f9ffc8a7ba3382a2d3328607b992ac
sammyv17 <vasquezsam@rocketmail.com>
2020-10-11 16:06:04 -0500

    Added sawblade prefab

---
+++ Assets/Sam/Assets/sawblade/spin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+
+    public float spinSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(0, -1 * (spinSpeed * Time.deltaTime), 0, Space.Self);
+    }
+}
