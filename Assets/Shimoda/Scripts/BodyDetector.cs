e90a09c6010adc55cd1fbec5523f5eb79145fa51
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2020-10-11 18:47:37 -0400

    Implementing Pushing

---
+++ Assets/Shimoda/Scripts/BodyDetector.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BodyDetector : MonoBehaviour
+{
+    public UnityEvent EnterSunlight;
+    public UnityEvent LeaveSunlight;
+    public UnityEvent NearSwitch;
+    public UnityEvent AwayFromSwitch;
+    public string[] switchTags;
+    public string sunlightTag;
+    private string switchie;
+    void Start()
+    {
+        if(EnterSunlight == null) EnterSunlight = new UnityEvent();
+        if(LeaveSunlight == null) LeaveSunlight = new UnityEvent();
+        if(NearSwitch == null) NearSwitch = new UnityEvent();
+        if(AwayFromSwitch == null) AwayFromSwitch = new UnityEvent();
+        foreach(string sth in switchTags){
+            switchie +=  "|" + sth + "|";
+        }
+
+    }
+    private void OnTriggerEnter(Collider other){
+        //Debug.Log("TriggerEnter: " + other.tag);
+         if(switchie.IndexOf("|"+other.tag+"|") >= 0){
+            if(NearSwitch != null) NearSwitch.Invoke();
+        }else if(other.tag.Equals(sunlightTag)){
+            if(EnterSunlight != null) EnterSunlight.Invoke();
+        }
+
+
+    }
+    private void OnTriggerExit(Collider other){
+        //Debug.Log("TriggerExit: " + other.tag);
+        if(switchie.IndexOf("|"+other.tag+"|") >= 0){
+            if(AwayFromSwitch != null) AwayFromSwitch.Invoke();
+        } else if(other.tag.Equals(sunlightTag)){
+            if(LeaveSunlight != null) LeaveSunlight.Invoke();
+        }
+    }
+
+}
---
+++ Assets/Shimoda/Scripts/FrontDetector.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FrontDetector : MonoBehaviour
+{
+    public string pushableTag;
+    public UnityEvent EnterPushable;
+    public UnityEvent LeavePushable;
+    public Rigidbody pushableBody;
+
+    void Start(){
+        if(EnterPushable == null) EnterPushable = new UnityEvent();
+        if(LeavePushable == null) LeavePushable = new UnityEvent();
+
+    }
+    private void OnTriggerEnter(Collider other){
+        if(other.tag == pushableTag){
+            Debug.Log("There is a pushable object in front");
+            pushableBody = other.GetComponent<Rigidbody>();
+            pushableBody.constraints = RigidbodyConstraints.FreezeRotationX | 
+                                       RigidbodyConstraints.FreezeRotationY | 
+                                       RigidbodyConstraints.FreezeRotationZ | 
+                                       RigidbodyConstraints.FreezePositionY | 
+                                       RigidbodyConstraints.FreezePositionZ;
+            if(EnterPushable != null) EnterPushable.Invoke();
+        } 
+    }
+    private void OnTriggerExit(Collider other){
+        if(other.tag == pushableTag){
+            Debug.Log("Left Pushable object realm");
+            pushableBody.constraints = RigidbodyConstraints.FreezeRotationX | 
+                                       RigidbodyConstraints.FreezeRotationY | 
+                                       RigidbodyConstraints.FreezeRotationZ | 
+                                       RigidbodyConstraints.FreezePositionX | 
+                                       RigidbodyConstraints.FreezePositionY | 
+                                       RigidbodyConstraints.FreezePositionZ;
+            pushableBody = null;
+            if(LeavePushable != null) LeavePushable.Invoke();
+        } 
+    }
+
+}
