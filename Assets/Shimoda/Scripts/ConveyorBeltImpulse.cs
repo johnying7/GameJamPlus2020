3d3da29ba77918a566f31d27fa32f1bc6a1a3343
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2020-10-11 21:38:22 -0400

    ConveyorBelt

---
+++ Assets/Shimoda/Scripts/ConveyorBeltImpulse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBeltImpulse : MonoBehaviour
+{
+    public float conveyorBeltImpulse;
+    //public bool right;
+    private void OnTriggerStay(Collider other){
+        Debug.Log("Tag: " + other.tag);
+        if(other.tag.Equals("PlayerGroundTrigger")){
+            // Applies force to the rigidbody
+            Debug.Log("Applying force");
+            other.transform.parent.GetComponent<Rigidbody>().AddForce(new Vector3(conveyorBeltImpulse, 0, 0 ));
+        }
+    }
+    private void OnTriggerExit(Collider other){
+        Debug.Log("Tag: " + other.tag);
+    }
+
+}
